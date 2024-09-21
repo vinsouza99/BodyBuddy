12a7c3aa7e9c8f90283b5d9bf6fda136e111ae87
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-09-21 15:14:38 -0700

    Deleted unnecessary comments

--- server/database.js
+++ server/database.js
@@ -15,18 +15,3 @@ sequelize.authenticate()
   });
 
 export default sequelize;
-
-
-// import pg from 'pg';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const { Pool } = pg;
-
-// export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
