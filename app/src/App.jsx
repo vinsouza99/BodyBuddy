7866cc52f42729c44a62a8af70463c5fdde2843d
v1n1c1u <vinicius.abner1039@gmail.com>
2024-09-26 15:12:46 -0700

    Removing browser router from TrainingProgram.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,53 +1,67 @@
-import './App.css'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate} from 'react-router-dom'
-import { MainLayout } from './layouts/MainLayout';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
-import { Dashboard } from './pages/Dashboard';
-import { Profile } from './pages/Profile';
-import { Routine } from './pages/Routine';
-import { NotFound } from './pages/NotFound';
-import { useAuth } from './AuthProvider';
-import { CircularProgress } from '@mui/material';
+import "./App.css";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Dashboard } from "./pages/Dashboard";
+import { Profile } from "./pages/Profile";
+import { Routine } from "./pages/Routine";
+import { NotFound } from "./pages/NotFound";
+import { useAuth } from "./AuthProvider";
+import { CircularProgress } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import TrainingProgram from "./pages/TrainingProgram";
 
 function App() {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return (
-      <CircularProgress />
-    );
+    return <CircularProgress />;
   }
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <>        
-      <Route index element={user ? <Navigate to="/dashboard" /> : <SignIn />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
+      <>
+        <Route
+          index
+          element={user ? <Navigate to="/dashboard" /> : <SignIn />}
+        />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
 
-      {/* Authentication required (MainLayout is applied) */}
-      <Route path='/' element={user ? <MainLayout /> : <Navigate to="/signin" />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<NotFound />} />
-      </Route>
+        {/* Authentication required (MainLayout is applied) */}
+        <Route
+          path="/"
+          element={user ? <MainLayout /> : <Navigate to="/signin" />}
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/training" element={<TrainingProgram />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
 
-      {/* Authentication required (MainLayout is NOT applied) */}
-      <Route path='/routine' element={user ? <Routine /> : <Navigate to="/signin" />} />
-    </>
+        {/* Authentication required (MainLayout is NOT applied) */}
+        <Route
+          path="/routine"
+          element={user ? <Routine /> : <Navigate to="/signin" />}
+        />
+      </>
     )
   );
 
   return (
     // Wrapped with ThemeProvider to apply theme.js styles
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  )
-  
+  );
 }
 
-export default App
+export default App;
--- app/src/pages/TrainingProgram.jsx
+++ app/src/pages/TrainingProgram.jsx
@@ -1,21 +1,11 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CreateWorkout } from './CreateWorkout';
-
+import React from "react";
 
 const TrainingProgram = () => {
-    return (
-      <div>
-          <h1>TrainingProgram</h1>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<CreateWorkout />} />
-              </Routes>
-          </BrowserRouter>
-      </div>
-  
-    )
-  }
-  
-  export default TrainingProgram;
-  
+  return (
+    <div>
+      <h1>TrainingProgram</h1>
+    </div>
+  );
+};
+
+export default TrainingProgram;
