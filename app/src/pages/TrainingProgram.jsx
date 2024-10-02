4e97c16d01449f6a09af92d395eddbe762b40bfa
terustestacount <terumasamori@gmail.com>
2024-10-01 21:42:47 -0700

    Calling "TrainingCard" component on "TrainingProgram.jsx"
    This is work in progress. I'm trying to call "TrainingCard" component on "TrainingProgram.jsx"

--- app/src/pages/TrainingProgram.jsx
+++ app/src/pages/TrainingProgram.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { useEffect } from "react";
+<<<<<<< Updated upstream
 import { setPageTitle } from "../utils";
+=======
+<<<<<<< Updated upstream
+import { setPageTitle } from "../utils/utils";
+=======
+import { setPageTitle } from "../utils";
+import TrainingCard from "../components/TrainingCard"
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 const TrainingProgram = (props) => {
   useEffect(() => {
@@ -9,6 +18,7 @@ const TrainingProgram = (props) => {
   return (
     <div>
       <h1>TrainingProgram</h1>
+      <TrainingCard/>
     </div>
   );
 };
