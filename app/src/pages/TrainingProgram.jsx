7c61bd0a93610ad05dbb88047efc25045d4342b6
cocoysg <cocoy.suguitan@yahoo.com>
2024-10-03 21:00:37 -0700

    Display Routines and Start Routines

--- app/src/pages/TrainingProgram.jsx
+++ app/src/pages/TrainingProgram.jsx
@@ -1,20 +1,40 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { setPageTitle } from "../utils/utils";
-import TrainingCard from "../components/TrainingCard";
+import { getAllPresetRoutines } from "../controllers/RoutineController"; // Cocoy: Import the function to display Routines
+import TrainingCard from "../components/TrainingCard"; // Cocoy: Load TrainingCard component
+import { Typography } from "@mui/material";
 
 export const TrainingProgram = (props) => {
+  const [routines, setRoutines] = useState([]); // Cocoy: Declare a state variable to hold the list of routines and a function to update it
+
   useEffect(() => {
     setPageTitle(props.title);
+
+    const loadRoutines = async () => {
+      const presetRoutines = await getAllPresetRoutines(); // Cocoy: Load preset routines
+      setRoutines(presetRoutines); // Cocoy: update the state with the loaded routines
+    };
+
+    loadRoutines();
   }, []);
 
   return (
     <div>
-      <h1>TrainingProgram</h1>
-      <TrainingCard />
+      <h1>Training</h1>
+      <div>
+        <Typography variant="h4" align="left">
+          Routines
+        </Typography>
+        {/* Create TrainingCard for each routine */}
+        {routines.map((routine) => (
+          <TrainingCard key={routine.id} routine={routine} />
+        ))}
+      </div>
     </div>
   );
 };
+
 TrainingProgram.propTypes = {
   title: PropTypes.string,
 };
