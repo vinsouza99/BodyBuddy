58be2c29e1cb75d577fa83296d4f555593218326
cocoysg <cocoy.suguitan@yahoo.com>
2024-10-09 21:41:52 -0700

    Update Dashboard Layout

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 export const Footer = () => {
   return (
-    <div className="footer">©2024 BodyBuddy</div>
-  )
-}
+    <Box display="flex" justifyContent="center" sx={{ padding: 4 }}>
+      <Typography variant="body2" color="text.primary">
+        <Link href="#" sx={{ marginRight: 2 }}>
+          Privacy Policy
+        </Link>
+        <Link href="#" sx={{ marginRight: 2 }}>
+          Terms of Service
+        </Link>
+      </Typography>
+      <Typography variant="body2" color="text.primary" sx={{ marginRight: 2 }}>
+        Version 1.0
+      </Typography>
+      <Typography variant="body2" color="text.primary">
+        ©2024 BodyBuddy
+      </Typography>
+    </Box>
+  );
+};
