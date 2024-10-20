e7d2105ba93f2e50dda8c417696f9d3268b30bd5
terustestacount <terumasamori@gmail.com>
2024-10-19 21:38:02 -0700

    Adding WallOf Fame Component
    Adding WallOf Fame Component on UserProfile.jsx

--- app/src/components/UserInfo.jsx
+++ app/src/components/UserInfo.jsx
@@ -11,6 +11,7 @@ import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import Chip from "@mui/material/Chip";
 import PropTypes from "prop-types";
+import { WallOfFame } from './WallOfFame';
 
 
 
@@ -35,21 +36,39 @@ function UserInfo(props) {
 return (
   <>
 
-  <Card sx={{ marginLeft: 2, padding: 3, borderRadius: 2}}>
+  <Card sx={{ padding: 3, borderRadius: 2, width: 1}}>
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <Avatar alt="Haley Grimes" src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100 }} />
     </Box>
 
     <Typography variant="h3" component="p" align='center' sx={{ margin: 3 }}>
-      {user.first_name} {user.last_name}
+    Haley Grimes
+      {/* {user.name} */}
     </Typography>
-            <Typography variant="body2" component="p">
-              Birthday: {user.birthday}
-            </Typography>
+    <Typography variant="body2" component="p">
+      {/* Birthday: {user.birthday} */}
+      Birthday: 1988-10-25
+    </Typography>
+    <Typography variant="body2" component="p">
+      {/* BWeight: {user.weight} */}
+      Weight: 144 Lbs
+    </Typography>
+    <Box sx={{display: 'flex', alignItems: 'flex-start', flexDirection: 'column'}}>
+      {/* <Typography>`Primary Goal: {user.goal}`</Typography> */}
+      <Typography>Primary Goal</Typography>
+
+      <Typography>Exercise frequency</Typography>
+      <Typography>Exercise intensity</Typography>
+      <Typography>When can you exercise</Typography>
+
+    </Box>
 
 
-      <Grid>
-        <Box sx={{ padding: 3, display: "flex", gap: 5, justifyContent: "center", borderBottom: 1 }}>
+
+      <Grid sx={{ marginTop: 3, borderTop: 1 }}>
+        <WallOfFame />
+
+        {/* <Box sx={{ padding: 3, display: "flex", gap: 5, justifyContent: "center", borderBottom: 1 }}>
           <CardContent>
               <Typography variant="body2" component="p">
                         Reach Goals
@@ -72,13 +91,13 @@ return (
                         /Weeks
               </Typography>
           </CardContent>
-        </Box>
+        </Box> */}
       </Grid>
 
-      <Box sx={{ display: "flex", gap: 5, justifyContent: "center", margin: 3 }}>
+    {/* <Box sx={{ display: "flex", gap: 5, justifyContent: "center", margin: 3 }}>
       <Chip label="SETTING" variant="outlined" onClick={handleClick} />
       <Chip label="LOGOUT" variant="outlined" onClick={handleClick} />
-    </Box>
+    </Box> */}
 
   </Card>
 
