ed41718142b7bbeb2a8a105b4889c492100a9a43
v1n1c1u <vinicius.abner1039@gmail.com>
2024-10-22 17:10:12 -0700

    Adding controlle methods for MuscleGroup and ExerciseType

--- app/src/controllers/LocalTablesController.js
+++ app/src/controllers/LocalTablesController.js
@@ -2,11 +2,15 @@ import axiosClient from "../utils/axiosClient";
 import Goal from "../models/Goal";
 import Intensity from "../models/Intensity";
 import Achievement from "../models/Achievement";
+import ExerciseType from "../models/ExerciseType";
+import MuscleGroup from "../../../server/models/MuscleGroup";
 
 const API_ROUTE = "local";
 const API_GOALS_ROUTE = "goals";
 const API_INTENSITY_ROUTE = "intensity";
 const API_ACHIEVEMENT_ROUTE = "achievements";
+const API_MUSCLE_GROUPS_ROUTE = "muscleGroups";
+const API_TYPE_ROUTE = "types";
 
 const getAllGoals = async () => {
   try {
@@ -69,10 +73,74 @@ const getAllAchievements = async () => {
     console.log(e);
   }
 };
+const getAchievement = async (achievement_id) => {
+  try {
+    const response = await axiosClient.get(
+      `${API_ROUTE}/${API_ACHIEVEMENT_ROUTE}/${achievement_id}`
+    );
+    const data = await response.data;
+    return new Achievement(data.id, data.name, data.description);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getAllExerciseTypes = async () => {
+  try {
+    const response = await axiosClient.get(`${API_ROUTE}/${API_TYPE_ROUTE}`);
+    const data = await response.data;
+    return data.map((type) => new ExerciseType(type.id, type.name));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getExerciseType = async (type_id) => {
+  try {
+    const response = await axiosClient.get(
+      `${API_ROUTE}/${API_TYPE_ROUTE}/${type_id}`
+    );
+    const type = await response.data;
+    return new ExerciseType(type.id, type.name);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getAllMuscleGroups = async () => {
+  try {
+    const response = await axiosClient.get(
+      `${API_ROUTE}/${API_MUSCLE_GROUPS_ROUTE}`
+    );
+    const data = await response.data;
+    return data.map(
+      (muscleGroup) => new MuscleGroup(muscleGroup.id, muscleGroup.name)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+const getMuscleGroup = async (muscle_group_id) => {
+  try {
+    const response = await axiosClient.get(
+      `${API_ROUTE}/${API_MUSCLE_GROUPS_ROUTE}/${muscle_group_id}`
+    );
+    const muscleGroup = await response.data;
+    return new MuscleGroup(muscleGroup.id, muscleGroup.name);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 export {
   getAllGoals,
   getGoal,
   getAllIntensities,
   getIntensity,
   getAllAchievements,
+  getAchievement,
+  getAllExerciseTypes,
+  getExerciseType,
+  getAllMuscleGroups,
+  getMuscleGroup,
 };
