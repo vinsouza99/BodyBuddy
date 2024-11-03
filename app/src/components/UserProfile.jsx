6537bb19659eb5e00f3051945c68b6da733a20c8
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-11-02 20:04:11 -0700

    Revised filter func in Profile  page

--- app/src/components/UserProfile.jsx
+++ app/src/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
-import { Box, Typography, Avatar, LinearProgress } from "@mui/material";
+import { Box, Typography, Avatar } from "@mui/material";
+import { UserProgressBar } from "./UserProgressBar";
 
 export const UserProfile = ({
   username = "",
@@ -15,8 +16,6 @@ export const UserProfile = ({
         width: "100%",
         gap: 2,
         alignItems: "center",
-        marginTop: 2.5,
-        marginBottom: 2.5,
       }}
     >
       {profilePicture ? (
@@ -35,56 +34,18 @@ export const UserProfile = ({
           {username.charAt(0)}
         </Avatar>
       )}
-      <Box sx={{ flex: 1, display: "flex", flexDirection: "column", gap: 2, }}>
+      <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
         <Typography
           textAlign="left"
           sx={{ fontWeight: "bold", fontSize: "1.5rem" }} 
         >
           Hi, {username}!
         </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: { xs: "column", lg: "row" },
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography textAlign="left">Level {level}</Typography>
-          <Typography textAlign="left">
-            Earn {100 - levelProgress} points to level up
-          </Typography>
-        </Box>
         <Box sx={{ width: "100%", position: "relative" }}>
-          <LinearProgress
-            variant="determinate"
-            value={levelProgress}
-            valueBuffer={100}
-            sx={{
-              "--LinearProgress-radius": "8px",
-              "--LinearProgress-progressThickness": "30px",
-              height: "20px",
-              boxShadow: "sm",
-              borderRadius: "15px",
-              "& .MuiLinearProgress-bar": {
-                backgroundColor: "primary.main",
-              },
-              backgroundColor: "grey.300",
-            }}
-          ></LinearProgress>
-          <Typography
-            color="text.primary"
-            sx={{
-              position: "absolute",
-              top: 0,
-              left: "50%",
-              transform: "translateX(-50%)",
-              width: "100%",
-              textAlign: "center",
-              lineHeight: "20px",
-            }}
-          >
-            {`${levelProgress} / 100`}
-          </Typography>
+          <UserProgressBar
+            level={level}
+            levelProgress={levelProgress}
+          />
         </Box>
       </Box>
     </Box>
--- app/src/pages/Profile.jsx
+++ app/src/pages/Profile.jsx
@@ -1,32 +1,39 @@
+import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { setPageTitle } from "../utils/utils";
+import { Backdrop, Box, CircularProgress, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import UserInfo from "../components/UserInfo";
 import History from "../components/History";
 import { getUser, getUserHistory } from "../controllers/UserController";
 import { useAuth } from "../utils/AuthProvider";
-import historyData from "../components/HistoryData.json";
 import { useNotifications } from "@toolpad/core/useNotifications";
 
 export const Profile = (props) => {
-  const [currentUser, setCurrentUser] = useState({});
-  const [history, setHistory] = useState(historyData);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [history, setHistory] = useState([]);
   const { user } = useAuth(); // For session management
   const notifications = useNotifications();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setPageTitle(props.title);
 
     async function getUserData() {
-      const userData = await getUser(user);
-      setCurrentUser(userData);
-      console.log(userData);
+      try {
+        const userData = await getUser(user);
+        setCurrentUser(userData);
+        console.log(userData);
 
-      const userHistoryData = await getUserHistory(user.id);
-      setHistory(userHistoryData);
-      console.log(userHistoryData);
+        const userHistoryData = await getUserHistory(user.id);
+        setHistory(userHistoryData);
+        console.log(userHistoryData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     }
-
     getUserData();
   }, []);
 
@@ -37,8 +44,21 @@ export const Profile = (props) => {
   }, []);
   return (
     <>
-      {Object.keys(currentUser).length > 0 ? (
-        <Grid container spacing={1}>
+      {/* Backdrop for loading */}
+      <Backdrop
+        open={loading}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Box textAlign="center">
+          <CircularProgress color="inherit" />
+          <Typography variant="h6" sx={{ mt: 2 }}>
+            Loading...
+          </Typography>
+        </Box>
+      </Backdrop>
+
+      {currentUser && history ? (
+        <Grid container spacing={2}>
           <Grid size={{ xs: 12, md: 6 }} display={"flex"}>
             <UserInfo user={currentUser} />
           </Grid>
@@ -52,3 +72,7 @@ export const Profile = (props) => {
 };
 
 export default Profile;
+
+Profile.propTypes = {
+  title: PropTypes.string,
+};
