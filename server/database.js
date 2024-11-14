0b51142f129e19b14e8afefb0178fd5fc39b7177
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-11-13 20:56:07 -0800

    Modification for deployment to Vercel

--- server/database.js
+++ server/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres", // or 'mysql', 'mariadb', 'sqlite'
+  dialectModule: pg,
 });
 
 // Connect DB
